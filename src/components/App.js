96fe0a2ed7d27ab2b65bb9a4b71a747b5ac408bd
Dhirendra-byte <68893214+Dhirendra-byte@users.noreply.github.com>
2020-10-21 21:46:39 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,19 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+		super(props);
+		this.greetUser = this.greetUser.bind(this);
+	  
 	};
 
+	greetUser(){
+return <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+}
     render() {
     	return(
     		<div id="main">
 				{ /* Do not remove this main div!! */ }
+                <button id="click" onClick={this.greetUser}>click</button>
     		</div>
     	);
     }
