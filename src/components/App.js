4e1e51961b3981d96a70c43936036247401884c9
Dhirendra-byte <68893214+Dhirendra-byte@users.noreply.github.com>
2020-11-21 09:05:34 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,30 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-constructor(){
-  super()
-this.state={msg:""}
-}
+  constructor() {
+    super();
+    this.state = { msg: "" };
+  }
 
-Clicked(){
-this.setState({
-msg:"Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
-})
-}
-    
-    render() {
-    	return(
-    		<div id="main">
-				<p id="para">{this.state.msg}</p>
-			<button id="click" onClick={this.Clicked()}>click here</button>
-    		</div>
-    	);
-    }
-}
+  Clicked() {
+    this.setState({
+      msg:
+        "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+    });
+  }
 
+  render() {
+    return (
+      <div id="main">
+        {/* Do not remove this main div!! */}
+        <p id="para">{this.state.msg}</p>
+        <button id="click" onClick={() => this.Clicked()}>
+          click here
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
-
